ab0372d4b4afb67516c72a6821a187bc5599386f
Valeria Andreeva <you@example.com>
2022-09-17 23:03:58 +0300

    Добавила решение Task01 file

---
+++ Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите два числа и мы узнаем яляется ли первое число квадратом второго");
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int squareb = b * b;
+if (squareb == a)
+{
+    Console.Write($"Да, первое число {a} - это квадрат второго числа {b}");
+
+}
+else
+{
+    Console.Write($"Нет, певрое число {a} - это не квадрат второго числа {b}");
+}
